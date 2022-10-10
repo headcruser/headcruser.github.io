27ed7a564046e649774f4980d04cf4881edc92bb
headcruser <headcruser@gmail.com>
2022-10-09 19:48:51 -0500

    Correccion Funcionalidad envio de correo
    - Se agrega loading
    - Se agrega modal para mostrar mensaje de agradecimiento
    - Se agrega boton scroll
    - Se corrije script para envio de correo

--- js/app.js
+++ js/app.js
@@ -1,20 +1,36 @@
 //* FORMULARIO DE CONTACTO
 ((d,w)=>{
 	const $form = d.querySelector('.form--contact');
+	const $loader = d.querySelector('.contact-form-loader');
+	const $response = d.querySelector('.contact-form-response');
+	const $btn_submit = d.querySelector('input[type="submit"]');
 
 	$form.addEventListener('submit',function(e){
 		e.preventDefault();
+		$btn_submit.classList.add('none');
+		$loader.classList.remove('none');
 
-		fetch('https://formsubmit.co/ajax/headcruser@gmail.com',{
+		fetch('https://formsubmit.co/ajax/054d237dfd9e2a9f6557bad1e588670e',{
 			method:'POST',
 			body: new FormData(e.target)
 		})
 		.then(res => res.ok ? res.json(): Promise.reject(res) )
 		.then(json => {
-			console.log(json);
+			location.hash = '#gracias';
 			$form.reset();
 		})
-		.catch( err => err)
+		.catch( err => {
+			let message = err.statusText || 'Ocurrio un error al enviar, intenta nuevamente =s';
+            $response.querySelector('h3').innerHTML = `Error ${err.status}: ${message}`
+		})
+		.finally(() => {
+            $loader.classList.add('none');
+			$btn_submit.classList.remove('none');
+
+            setTimeout(() => {
+				location.hash = '#close';
+            }, 1500);
+        })
 	});
 
 	// * SCROLL BUTTON
